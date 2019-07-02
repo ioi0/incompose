b07ddc87b4fb18ae79a4e593f4d7ba432dfe1c72
Roman Zanettin <roman.zanettin@gmail.com>
2019-07-02 22:29:40 +0200

    refactored HoCs to make them composable again

--- src/withLifecycle.js
+++ src/withLifecycle.js
@@ -2,23 +2,10 @@
  * @author recompose (https://github.com/acdlite/recompose)
  */
 
-import { createClass } from 'inferno-create-class';
-import createHelper from './createHelper';
+export default lifecycles => Component => {
+	Component.defaultHooks = {
+		...lifecycles,
+	};
 
-const lifecycle = spec => BaseComponent => {
-	if (process.env.NODE_ENV !== 'production' && spec.render) {
-		console.error(
-			'lifecycle() does not support the render method; its behavior is to ' +
-				'pass all props and state to the base component.',
-		);
-	}
-
-	return createClass({
-		...spec,
-		render() {
-			return <BaseComponent {...this.props} />;
-		},
-	});
+	return Component;
 };
-
-export default createHelper(lifecycle, 'lifecycle');
